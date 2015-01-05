daaf5bdc1bd24a6c4ae87e856493e09e65eb8de1
Crestington <Zelus2009@hotmail.com>
2015-01-04 22:01:31 -0800

    Spelling error - Stable Copy

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -27,7 +27,7 @@ namespace Checkpoints
         ( 0,      hashGenesisBlock )
         ( 100,    uint256("0x0000006f6d50428c4b20aed705454e59001d41526c2a0bec3baf9bc5c17df9c7"))
         ( 2500,   uint256("0x000000102635b1390c240e0072001b752f7a3d793230d121054e7c3acb53bf66"))
-		( 8200,   unit256("0x33968e2be1926a531fa39e8040d79274b2ae7448a6b6f6ec7827ca026c741f0f"))
+		( 8200,   uint256("0x33968e2be1926a531fa39e8040d79274b2ae7448a6b6f6ec7827ca026c741f0f"))
     ;
 
     // TestNet has no checkpoints
