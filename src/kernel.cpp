a934c68282500367b8346d2ee513b0a215de7dd5
Crestington <Zelus2009@hotmail.com>
2015-02-03 07:20:35 -0800

    stake modifier

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -20,13 +20,13 @@ unsigned int nModifierInterval = MODIFIER_INTERVAL;
 // Hard checkpoints of stake modifiers to ensure they are deterministic
 static std::map<int, unsigned int> mapStakeModifierCheckpoints =
     boost::assign::map_list_of
-        ( 0, 0x0e00670bu )
+        ( 0, 0xfd11f4e7u )
     ;
 
 // Hard checkpoints of stake modifiers to ensure they are deterministic (testNet)
 static std::map<int, unsigned int> mapStakeModifierCheckpointsTestNet =
     boost::assign::map_list_of
-        ( 0, 0x0e00670bu )
+        ( 0, 0xfd11f4e7u )
     ;
 
 // Get time weight
